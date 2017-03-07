ad7803e7def035bb98cf71f40f59e818f4bf874e
Seksan Neramitthanasombat <sek.cgnz@gmail.com>
2017-03-07 09:57:08 +0700

    Front-end web app

---
+++ dms-app/src/main/java/soa/group6/dms/DmsAppApplication.java
@@ -0,0 +1,12 @@
+package soa.group6.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsAppApplication.class, args);
+	}
+}
