0b7b23b7f31b4012ad16ad83086b86a52eaf72f6
Seksan Neramitthanasombat <sek.cgnz@gmail.com>
2017-04-03 09:55:04 +0700

    Add 404 page

---
+++ docu-pool/src/components/pages/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="hero is-info is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">
+          404 Not Found
+        </h1>
+        <h2 class="subtitle">
+          {{ $route.path }}
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'notFound'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
